695e5e879b80ff4082ebe4a15284d1cd96dab6e6
FelipeNarciso <luisfelipenb88@gmail.com>
2024-09-10 10:56:46 -0300

    Página login finalizada

---
+++ front-end/innovation-academy/src/Components/FormCadastro.jsx
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+import logoNome from "../Images/Login/LogoNome.png";
+import iconeGithub from "../Images/Login/github-icon.png";
+import quebraLinha from "../Images/Login/quebra-linha.png";
+
+import "@fontsource/poppins";
+
+const LogoNome = styled.img`
+  width: 60%;
+  padding-bottom: 8%;
+`;
+
+const TituloForm = styled.span`
+  font-size: 1rem;
+  padding-bottom: 5%;
+`;
+
+const Input = styled.input`
+  color: white;
+  background-color: RGB(42, 42, 44);
+  border: 1px solid transparent;
+  padding: 3%;
+  border-radius: 5px 5px 5px;
+  margin-bottom: 5%;
+
+  &:focus {
+    border-color: RGB(10, 189, 98);
+    outline: none;
+  }
+`;
+
+const LabelInput = styled.span`
+  padding-bottom: 2%;
+  font-size: 12px;
+`;
+
+const Termos = styled.span`
+  width: fit-content;
+  color: white;
+  padding-bottom: 3%;
+  transition: 0.3s ease;
+  font-size: 12px;
+
+  a{
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+  }
+
+`;
+
+const BtnCriar = styled.button`
+  background-color: RGB(10, 189, 98);
+  border: none;
+  padding: 3%;
+  font-family: "Poppins", sans-serif;
+  margin-bottom: 3%;
+  color: white;
+  border-radius: 5px 5px 5px;
+  font-size: 15px;
+  transition: 0.3s ease;
+
+  &:hover {
+    cursor: pointer;
+    background-color: rgb(23, 155, 87);
+  }
+`;
+
+const DivBtnGithub = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+`;
+
+const IconeGithub = styled.img`
+  margin-right: 3%;
+  transition: 0.3s ease;
+`;
+
+const BtnGithub = styled.button`
+  margin-left: 5%;
+  font-family: "Poppins", sans-serif;
+  border-radius: 5px 5px 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  background-color: RGB(42, 42, 44);
+  border: none;
+  color: white;
+  padding: 2%;
+  font-size: 12px;
+  transition: 0.3s ease;
+
+  &:hover {
+    cursor: pointer;
+    background-color: rgb(36, 36, 37);
+  }
+
+  &:hover ${IconeGithub} {
+    transform: rotate(45deg);
+  }
+`;
+
+const QuebraLinha = styled.img`
+  margin-top: 3%;
+`;
+
+function FormCadastro() {
+  return (
+    <>
+      <LogoNome src={logoNome} alt="" srcSet="" />
+      <TituloForm>Crie sua conta gratuitamente</TituloForm>
+      <LabelInput>Nome</LabelInput>
+      <Input type="text" />
+      <LabelInput>Email</LabelInput>
+      <Input type="text" />
+      <LabelInput>Senha</LabelInput>
+      <Input type="password" />
+      <LabelInput>Repita sua senha</LabelInput>
+      <Input type="password" />
+      <Termos>Ao se cadastrar, você aceita os <a href="#">nossos termos de uso</a> e nossa <a href="#">política de privacidade</a></Termos>
+      <BtnCriar>Criar Conta</BtnCriar>
+      <DivBtnGithub>
+        <span>Ou se preferir</span>
+        <BtnGithub>
+          <IconeGithub src={iconeGithub} alt="" srcSet="" />
+          Entre com o Github
+        </BtnGithub>
+      </DivBtnGithub>
+      <QuebraLinha src={quebraLinha} alt="" srcSet="" />
+    </>
+  );
+}
+export default FormCadastro;
--- front-end/innovation-academy/src/Pages/Login.jsx
+++ front-end/innovation-academy/src/Pages/Login.jsx
@@ -1,28 +1,30 @@
 import styled from "styled-components";
+import { useState } from "react";
 import logo from "../Images/Login/Logo.png";
-import logoNome from "../Images/Login/LogoNome.png";
-import iconeGithub from '../Images/Login/github-icon.png'
-import quebraLinha from '../Images/Login/quebra-linha.png'
-import boracodar from '../Images/Login/boracodar.png'
-import iconLogin from '../Images/Login/icon-login.png'
+import boracodar from "../Images/Login/boracodar.png";
+import iconLogin from "../Images/Login/icon-login.png";
 import { GoChevronRight } from "react-icons/go";
-import '@fontsource/poppins';
+import "@fontsource/poppins";
+import FormCadastro from "../Components/FormCadastro";
+import FormLogin from "../Components/FormLogin";
 
 const Body = styled.div`
   color: white;
-  font-family: 'Poppins', sans-serif;
-  overflow: hidden;
+  font-family: "Poppins", sans-serif;
+  background-color: RGB(18, 18, 20);
+  overflow-x: hidden;
+  overflow-y: scroll;
   height: 100vh;
   display: flex;
   flex-wrap: wrap;
 
-  @media screen and (max-width: 900px) {
+  @media screen and (max-width: 768px) {
     justify-content: center;
-    overflow: scroll;
+    overflow-y: scroll;
   }
 
-  @media screen and (min-width: 901px){
-    overflow: hidden;
+  @media screen and (max-width: 1440px) {
+    overflow-y: scroll;
   }
 `;
 
@@ -33,13 +35,20 @@ const Box1 = styled.div`
   justify-content: center;
   align-items: center;
   background-color: RGB(18, 18, 20);
+  transition: transform 0.5s ease-in-out;
 
-  @media screen and (max-width: 900px) {
+  ${({ move }) =>
+    move &&
+    `
+    transform: translateX(70%);
+    
+  `}
+
+  @media screen and (max-width: 768px) {
     display: none;
   }
-  @media screen and (min-width:900 ){
-    display: flex;
-  }
+
+
 `;
 
 const Box2 = styled.div`
@@ -49,9 +58,23 @@ const Box2 = styled.div`
   background-color: white;
   width: 40%;
   background-color: RGB(26, 26, 30);
+  transition: transform 0.5s ease-in-out;
 
-  @media screen and (max-width: 900px) {
+  ${({ move }) =>
+    move &&
+    `
+    transform: translateX(-150%);
+    padding-top: 1%;
+  `}
+
+  @media screen and (max-width: 768px) {
     width: 100%;
+    transition: transform 1s ease-in-out;
+    ${({ move }) =>
+      move &&
+      `
+    transform: rotateY(360deg);
+  `}
   }
 
   @media screen and (max-width: 1024px) {
@@ -69,131 +92,31 @@ const ImgBoracodar = styled.img`
   width: 15%;
 `;
 
-// Conteudo Box2
-const LogoNome = styled.img`
-  width: 60%;
-  padding-bottom: 8%;
-`;
-
-const TituloForm = styled.span`
-  font-size: 18px;
-  padding-bottom: 5%;
-`;
-
-const Input = styled.input`
-color: white;
-  background-color: RGB(42, 42, 44);
-  border: 1px solid transparent;
-  padding: 3%;
-  border-radius: 5px 5px 5px;
-  margin-bottom: 5%;
-
-  &:focus{
-    border-color: RGB(10, 189, 98);
-    outline: none;
-  }
-`;
-
-const LabelInput = styled.span`
-  padding-bottom: 2%;
-  font-size: 12px;
-`;
-
-const EsqueciSenha = styled.span`
-width: fit-content;
- color: RGB(10, 189, 98);
- padding-bottom: 5%;
- transition: 0.3s ease;
- font-size: 12px;
-
- &:hover{
-  cursor: pointer;
-  color: rgb(23, 155, 87);
- }
-
-`;
-
-const BtnEntar = styled.button`
-  background-color: RGB(10, 189, 98);
-  border: none;
-  padding: 3%;
-  font-family: 'Poppins' , sans-serif;
-  margin-bottom: 5%;
-  color: white;
-  border-radius: 5px 5px 5px;
-  font-size: 15px;
-  transition: 0.3s ease;
-
-  &:hover{
-    cursor: pointer;
-    background-color: rgb(23, 155, 87);
-  }
-`;
-
-const DivBtnGithub = styled.div`
- display: flex;
- align-items: center;
- font-size: 12px;
-`;
-
-const IconeGithub = styled.img`
-  margin-right: 3%;
-  transition: 0.3s ease;
-`;
-
-const BtnGithub = styled.button`
-  margin-left: 5%;
-  font-family: 'Poppins', sans-serif;
-  border-radius: 5px 5px 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-  background-color: RGB(42, 42, 44);
-  border: none;
-  color: white;
-  padding: 2%;
-  font-size: 12px;
-  transition: 0.3s ease;
-
-  &:hover{
-    cursor: pointer;
-    background-color: rgb(36, 36, 37);
-  }
-
-  &:hover ${IconeGithub}{
-    transform: rotate(45deg);
-  }
-`;
-
-const QuebraLinha = styled.img`
-  margin-top: 8%;
-`;
-
+//Conteúdo Box2
 const ImgIconLogin = styled.img`
   transform: scale(0.8);
 `;
 const GoChevronRightStyled = styled(GoChevronRight)`
-  color:RGB(10, 189, 98);
+  color: RGB(10, 189, 98);
   transition: 0.3s ease;
 `;
 
 const DivBtnInscrever = styled.div`
-   background-color: RGB(42, 42, 44);
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   padding: 3%;
-   border-radius: 5px 5px 5px;
-   margin-top: 5%;
+  background-color: RGB(42, 42, 44);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 3%;
+  border-radius: 5px 5px 5px;
+  margin-top: 5%;
 
-   &:hover{
+  &:hover {
     cursor: pointer;
-   }
+  }
 
-   &:hover ${GoChevronRightStyled}{
-      transform: scale(1.5);
-   }
+  &:hover ${GoChevronRightStyled} {
+    transform: scale(1.5);
+  }
 `;
 
 const ConteudoBtnInscreverLeft = styled.div`
@@ -217,48 +140,74 @@ const Footer = styled.div`
   margin-bottom: 1%;
   margin-left: 1%;
   color: RGB(39, 39, 40);
+  @media screen and (man-width: 901px) {
+    display: none;
+  }
+
+  ${({ move }) =>
+    move &&
+    `
+   right: 0;
+   margin-bottom: 1%;
+   margin-right: 2%;
+  `}
 `;
 
-
-
-
-
 function Login() {
+  const [moveBoxes, setMoveBoxes] = useState(false);
+
+  const handleBoxes = () => {
+    if (!moveBoxes) {
+      setMoveBoxes(true);
+    } else {
+      setMoveBoxes(false);
+    }
+  };
+
   return (
     <Body>
-      <Box1>
+      <Box1 move={moveBoxes}>
         <ImgLogo src={logo} alt="" srcset="" />
         <ImgBoracodar src={boracodar} alt="" srcset="" />
       </Box1>
-      <Box2>
-        <LogoNome src={logoNome} alt="" srcset="" />
-        <TituloForm>Acesse sua conta</TituloForm>
-        <LabelInput>Email</LabelInput>
-        <Input type="text"/>
-        <LabelInput>Senha</LabelInput>
-        <Input type="password" />
-        <EsqueciSenha>Esqueci minha senha</EsqueciSenha>
-        <BtnEntar>Entrar</BtnEntar>
-        <DivBtnGithub>
-          <span>Ou se preferir</span>
-          <BtnGithub><IconeGithub src={iconeGithub} alt="" srcset="" />Entre com o Github</BtnGithub>
-        </DivBtnGithub>
-        <QuebraLinha src={quebraLinha} alt="" srcset="" />
-        <DivBtnInscrever>
-          <BtnInscreverLeft>
-            <ImgIconLogin src={iconLogin} alt="" />
-            <ConteudoBtnInscreverLeft>
-              <span>Não tem  uma conta?</span>
-              <SeInscreva>Se inscreva gratuitamente</SeInscreva>
-            </ConteudoBtnInscreverLeft>
-          </BtnInscreverLeft>
-
-          <div>
-            <GoChevronRightStyled />
-          </div>
-        </DivBtnInscrever>
+      <Box2 move={moveBoxes}>
+        {moveBoxes ? (
+          <>
+            <FormCadastro />
+            <DivBtnInscrever onClick={handleBoxes}>
+              <BtnInscreverLeft>
+                <ImgIconLogin src={iconLogin} alt="" />
+                <ConteudoBtnInscreverLeft>
+                  <span>Já tem uma conta?</span>
+                  <SeInscreva>Entre e aproveite o conteudo</SeInscreva>
+                </ConteudoBtnInscreverLeft>
+              </BtnInscreverLeft>
+              <div>
+                <GoChevronRightStyled />
+              </div>
+            </DivBtnInscrever>
+          </>
+        ) : (
+          <>
+            <FormLogin />
+            <DivBtnInscrever onClick={handleBoxes}>
+              <BtnInscreverLeft>
+                <ImgIconLogin src={iconLogin} alt="" />
+                <ConteudoBtnInscreverLeft>
+                  <span>Não tem uma conta?</span>
+                  <SeInscreva>Se inscreva gratuitamente</SeInscreva>
+                </ConteudoBtnInscreverLeft>
+              </BtnInscreverLeft>
+              <div>
+                <GoChevronRightStyled />
+              </div>
+            </DivBtnInscrever>
+          </>
+        )}
       </Box2>
-      <Footer><h5>@inovaTech</h5></Footer>
+      <Footer move={moveBoxes}>
+        <h5>@inovaTech</h5>
+      </Footer>
     </Body>
   );
 }
