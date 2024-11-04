a2111476a23d93e06fccdf4c28b15e18aeda0467
FelipeNarciso <luisfelipenb88@gmail.com>
2024-11-04 11:40:39 -0300

    melhorias modal recuperar senha

--- src/Components/Login/components/modal/PassRecoverModal.jsx
+++ src/Components/Login/components/modal/PassRecoverModal.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
-import { Link } from "react-bootstrap-icons";
+import { RiCloseLargeLine } from "react-icons/ri";
 import axios from "axios";
 
 const PassRecoverModal = ({ setEsqueceuSenha }) => {
   const [email, setEmail] = useState("");
+  const [emailEnviado, setEmailEnviado] = useState(false);
+
   const redefinirSenha = async () => {
     try {
-      await axios.post("http://localhost:8080/send-email", { email: email });
-      goToLogin();
+      axios.post("http://localhost:8080/send-email", { email: email });
+      setEmailEnviado(true);
     } catch (error) {
       console.error(error);
     }
@@ -24,97 +26,96 @@ const PassRecoverModal = ({ setEsqueceuSenha }) => {
         <Modal.Title
           style={{ textAlign: "center", width: "100%", color: "white" }}
         >
-          Redefinição de Senha
+          {emailEnviado ? (
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Email Enviado
+            </span>
+          ) : (
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Redefinição de Senha
+            </span>
+          )}
         </Modal.Title>
+        {emailEnviado && (
+          <RiCloseLargeLine
+            style={{ cursor: "pointer", color: "white" }}
+            onClick={goToLogin}
+          />
+        )}
       </Modal.Header>
       <Modal.Body style={{ backgroundColor: "rgb(27, 27, 31)" }}>
-        <Form
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            color: "white",
-          }}
-        >
-          <Form.Group
-            controlId="formTitle"
+        {!emailEnviado ? (
+          <Form
             style={{
-              marginBottom: "10px",
-              textAlign: "justify ",
-              justifyContent: "center",
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
-              width: "100%",
-              padding: "30px",
+              color: "white",
             }}
           >
-            <Form.Label style={{ marginBottom: "10px" }}>
-              Digite seu email no campo abaixo e lhe enviaremos uma nova senha:
-            </Form.Label>
-            <Form.Control
-              type="text"
-              name="title"
+            <Form.Group
+              controlId="formEmail"
               style={{
-                color: "var(--text-color)",
-                backgroundColor: "var(--main-color)",
-                border: "1px solid transparent",
-                padding: "3%",
-                borderRadius: "5px",
-                marginBottom: "5%",
+                marginBottom: "10px",
+                textAlign: "justify",
+                justifyContent: "center",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: "100%",
+                padding: "30px",
               }}
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-              onFocus={(event) => {
-                event.target.style.borderColor = "var(--contrast-color)";
-                event.target.style.outline = "none";
-              }}
-              onBlur={(event) => {
-                event.target.style.borderColor = "transparent";
-              }}
-            />
-          </Form.Group>
-          <Button
-            style={{ color: "white", backgroundColor: "rgb(9, 189, 96)" }}
-            onClick={redefinirSenha}
-          >
-            Redefinir senha
-          </Button>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer style={{ backgroundColor: "rgb(27, 27, 31)" }}>
-        <Modal.Title
-          style={{
-            fontSize: "14px",
-            textAlign: "center",
-            width: "100%",
-            color: "white",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              padding: "5px",
-              gap: "5px",
-            }}
-          >
-            Ja possui cadastro?
-            <p
-              style={{
-                color: "rgb(9, 189, 96)",
-                textDecoration: "underline",
-                cursor: "pointer",
-              }}
-              onClick={() => goToLogin()}
             >
-              Faça seu login.
-            </p>
+              <Form.Label style={{ marginBottom: "10px" }}>
+                Digite seu email no campo abaixo e lhe enviaremos uma nova
+                senha:
+              </Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                style={{
+                  color: "var(--text-color)",
+                  backgroundColor: "var(--main-color)",
+                  border: "1px solid transparent",
+                  padding: "3%",
+                  borderRadius: "5px",
+                  marginBottom: "5%",
+                }}
+                onChange={(event) => setEmail(event.target.value)}
+                onFocus={(event) => {
+                  event.target.style.borderColor = "var(--contrast-color)";
+                  event.target.style.outline = "none";
+                }}
+                onBlur={(event) => {
+                  event.target.style.borderColor = "transparent";
+                }}
+              />
+            </Form.Group>
+            <Button
+              style={{ color: "white", backgroundColor: "rgb(9, 189, 96)" }}
+              onClick={redefinirSenha}
+            >
+              Redefinir senha
+            </Button>
+          </Form>
+        ) : (
+          <div style={{ color: "white", textAlign: "center" }}>
+            <p>Por favor, verifique sua caixa de entrada.</p>
           </div>
-        </Modal.Title>
-      </Modal.Footer>
+        )}
+      </Modal.Body>
     </Modal>
   );
 };
