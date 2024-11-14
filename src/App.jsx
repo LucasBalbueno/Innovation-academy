fa5e47af4212e4419ec2dcac9228f05776e9c1d3
KAYSILVHA <kaysilvha@gmail.com>
2024-11-14 20:16:01 -0300

    removendo comentario

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,7 @@ function App() {
         <Route
           path="/home"
           element={
-            // <ProtectedRoute>
             <Home />
-            // </ProtectedRoute>
           }
         >
           <Route path="" element={<HomeContent />} />
